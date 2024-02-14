fa0ae37c679481e582afefcb80932473d4c8a6ae
Samarth Mali <113602046+samarthsm1100@users.noreply.github.com>
2024-02-14 18:23:21 +0000

    example edited in Astar

--- LP2/A2.py
+++ LP2/A2.py
@@ -1,7 +1,6 @@
 import heapq
 
 class Node:
-    #constructor
     def __init__(self, parent, position, g, h):
         self.parent = parent
         self.position = position
@@ -9,7 +8,6 @@ class Node:
         self.h = h
         self.f = g + h
 
-    # Comparison function to determine priority in the priority queue
     def __lt__(self, other):
         return self.f < other.f
 
@@ -19,18 +17,14 @@ def a_star(matrix, start_loc, end_loc):
     closed_set = set()
 
     start_node = Node(None, start_loc, 0, 0)
-    # Push the starting node to the open list with its priority
     heapq.heappush(open_list, (start_node.f, start_node))
                                     #[0]            [1]
     while open_list:
         current = heapq.heappop(open_list)[1]
-        # Add the current node's position to the closed_set
         closed_set.add(current.position)
 
-        # Check if we have reached the goal
         if current.position == end_loc:
             path = []
-            # Reconstruct the path by traversing the parent pointers
             while current:
                 path.append(current.position)
                 current = current.parent
@@ -42,7 +36,6 @@ def a_star(matrix, start_loc, end_loc):
         for direction in directions:
             new_position = (current.position[0] + direction[0], current.position[1] + direction[1])
 
-            # Check if the neighbor is out of bounds or blocked by an obstacle
             if (
                 new_position[0] < 0 or new_position[0] >= len(matrix) or
                 new_position[1] < 0 or new_position[1] >= len(matrix[0]) or
@@ -57,7 +50,6 @@ def a_star(matrix, start_loc, end_loc):
                 abs(new_position[1] - end_loc[1])
             )
 
-            # Check if position is already present in open list
             if any(node.position == new_node.position for _, node in open_list):
                 continue
 
@@ -72,7 +64,7 @@ matrix = [
     [0, 1, 0, 0, 0, 0],
     [0, 0, 1, 0, 0, 1],
     [0, 1, 0, 0, 0, 0],
-   
+    [0, 0, 0, 1, 1, 0],  
 ]
 
 # 1 -> obstacle ,0 -> empty space 
@@ -81,7 +73,7 @@ end_loc = (5,5)
 
 path = a_star(matrix, start_loc, end_loc)
 
-print(len(path))
+print("\nPath length is : ",len(path))
 if path:
     print("Path found:", path)
 else:
