eae932a7c4dd0dd58217a53d8340a45fefcd6cfe
Samarth Mali <113602046+samarthsm1100@users.noreply.github.com>
2024-03-11 03:27:33 +0000

    A1 modified

--- LP2/A1.cpp
+++ LP2/A1.cpp
@@ -1,123 +1,96 @@
-#include <iostream>
-#include <queue>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
- 
-struct Edge {
-    int src, dest;
+
+class edge{
+    public:
+    
+    int src;
+    int dest;
+    
+    edge(int s=0, int d=0){
+        src = s;
+        dest = d;
+    }
 };
- 
-class Graph
-{
-public:
+
+class graph{
+    public:
+    
     vector<vector<int>> adjList;
- 
-     Graph(vector<Edge> const &edges, int n)
-    {
+    
+    graph(vector<edge>edges, int n){
         adjList.resize(n);
- 
-        for (auto &edge: edges)
-        {
-            adjList[edge.src].push_back(edge.dest);
-            adjList[edge.dest].push_back(edge.src);
+        for(auto e:edges){
+            adjList[e.src].push_back(e.dest);
+            adjList[e.dest].push_back(e.src);
+        }
+    }
+    
+    void bfs(queue<int>&q, vector<bool>&discovered){
+        // base case
+        if(q.empty()){
+            return;
+        }
+        
+        int front = q.front();
+        q.pop();
+        cout << front << " ";
+        
+        for(int i : adjList[front]){
+            if(!discovered[i]){
+                discovered[i] = true;
+                q.push(i);
+            }
+        }
+        
+        bfs(q, discovered);
+    }
+    
+    void dfs(int v, vector<bool>&discovered){
+        discovered[v] = true;
+        cout << v << " ";
+        for (int u: adjList[v]){
+            if (!discovered[u]) {
+                dfs(u, discovered);
+            }
         }
     }
 };
- 
-void recursiveBFS(Graph const &graph, queue<int> &q, vector<bool> &discovered)
-{
-    if (q.empty()) {
-        return;
-    }
- 
-    int v = q.front();
-    q.pop();
-    cout << v << " ";
- 
-    for (int u: graph.adjList[v])
-    {
-        if (!discovered[u])
-        {
-            discovered[u] = true;
-            q.push(u);
-        }
-    }
- 
-    recursiveBFS(graph, q, discovered);
-}
 
-void DFS(Graph const &graph, int v, vector<bool> &discovered2)
-{
-    discovered2[v] = true;
- 
-    cout << v << " ";
- 
-    for (int u: graph.adjList[v])
-    {
-        if (!discovered2[u]) {
-            DFS(graph, u, discovered2);
-        }
-    }
-}
- 
-int main()
-{
 
-    //<=========== EXAMPLE 1 ===========>
-    vector<Edge> edges = {
+
+int main() {
+    
+    vector<edge> edges = {
         {1, 2}, {1, 3}, {1, 4}, {2, 5}, {2, 6}, {5, 9},
         {5, 10}, {4, 7}, {4, 8}, {7, 11}, {7, 12}
     };
- 
-    int n = 15;
- 
-    // vector <Edge> edges;
-    // int n;
-
-    // cout << "Enter number of nodes : ";
-    // cin >> n;
-
-    // while(true){
-    //     Edge e;
-    //     cin>>e.src>>e.dest;
-    //     edges.push_back(e);
-    //     char c; cin>>c;
-    //     if(c == 'N' || c == 'n'){
-    //         break;
-    //     }
-    // }
-
-    Graph graph(edges, n);
- 
-    vector<bool> discovered(n, false);
- 
-    queue<int> q;
     
-    cout << "BFS : ";
-    for (int i = 0; i < n; i++)
-    {
-        if (discovered[i] == false)
-        {
+    int n = 15;
+    
+    graph *g = new graph(edges, n);
+    
+    vector<bool>discovered(n,false);
+    queue<int>q;
+    
+    for(int i=0; i<n; i++){
+        if(discovered[i] == false){
             discovered[i] = true;
- 
             q.push(i);
- 
-            recursiveBFS(graph, q, discovered);
+            g->bfs(q,discovered);
         }
     }
-    cout << endl;
-
-    vector<bool> discovered2(n, false);
-
-    cout << "DFS : ";
-    for (int i = 0; i < n; i++)
-    {
-        if (discovered2[i] == false) {
-            DFS(graph, i, discovered2);
+    
+    cout << endl << endl;
+    
+    discovered.clear();
+    discovered.resize(n,false);
+    
+    for(int i=0; i<n; i++){
+        if(!discovered[i]){
+            g->dfs(i,discovered);
         }
     }
-    cout << endl;
- 
+    
     return 0;
 }
-
