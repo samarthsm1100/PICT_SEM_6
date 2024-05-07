623f40f7c3e3bb539319b3d2f68c0ce2506794f7
Samarth Mali <113602046+samarthsm1100@users.noreply.github.com>
2024-05-07 10:58:17 +0000

    ...

--- LP2/Demo.py
+++ LP2/Demo.py
@@ -1,20 +1,71 @@
-import random
-import re
+import heapq
 
-responses = {
-    'greetings': ['Hello, How can I help You ?', 'Greetings from Chatbot! How can I help you ?'],
-    'broken' : ['Broken Glass so buy a new one :)', 'Okay so your glass is broken as of your mind'],
-    'new' : ['yes, we have got variety of new laptops', 'What brand are you looking for ?']
-}
+class Node:
+    
+    def __init__(self, parent, position, g, h):
+        self.parent = parent
+        self.position = position
+        self.g = g
+        self.h = h
+        self.f = g+h
 
-def respond(query):
-    query = query.lower()
-    if re.search(r"\b(?:hello|hi)\b", query):
-        return random.choice(responses["greetings"])
-    elif re.search(r"\b(?:broken)\b", query):
-        return random.choice(responses["broken"])
-    else:
-        return "umm ?"
+    def __lt__(self, other):
+        return self.f < other.f
 
-ans = respond("Hi Sir")
-print(ans)
+def astar(matrix, start_loc, end_loc):
+    
+    openlist = []
+    closed_list = set()
+
+    start_node = Node(None, start_loc, 0, 0)
+    heapq.heappush(openlist, (start_node.f, start_node))
+
+    while openlist:
+
+        current = heapq.heappop(openlist)[1]
+        current_pos = current.position
+        closed_list.add(current_pos)
+
+        if(current_pos == end_loc):
+            path = []
+            while current:
+                path.append(current.position)
+                current = current.parent
+            path.reverse()
+            return path
+
+        directions = {(0,1), (0,-1), (1,0), (-1,0)}
+
+        for d in directions:
+            new_position = (current_pos[0]+d[0], current_pos[1]+d[1])
+            if(
+                new_position[0] < 0 or new_position[0] >= len(matrix)
+                or new_position[1] < 0 or new_position[1] >= len(matrix[0])
+                or matrix[new_position[0]][new_position[1]] == 1 or
+                new_position in closed_list 
+            ):
+                continue
+        
+            new_node = Node(current, new_position, current.g+1, abs(new_position[0]-current_pos[0])+abs(new_position[1]-current_pos[1]))
+
+            heapq.heappush(openlist, (new_node.f ,new_node))
+    
+    return path
+
+matrix = [
+    [0, 0, 0, 0, 0, 0],
+    [0, 1, 1, 0, 1, 0],
+    [0, 1, 0, 0, 0, 0],
+    [0, 0, 1, 0, 0, 1],
+    [0, 1, 0, 0, 0, 0],
+    [0, 0, 0, 1, 1, 0],  
+]
+
+# 1 -> obstacle ,0 -> empty space 
+start_loc = (0, 0) #round brackets!!!!
+end_loc = (5,5)
+
+path = astar(matrix, start_loc, end_loc)
+
+print(len(path))
+print(path)
